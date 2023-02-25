09384b72cb34aa4e07638db79a7182a88ff3d2eb
Ilya <veruhanov00@mail.ru>
2023-02-25 05:30:54 -0500

    add programm

---
+++ main.py
@@ -0,0 +1,32 @@
+from notes_add import notes_add
+from notes_add2 import notes_addContinue
+from notes_dateSelect import notes_dateSelect
+from notes_delete import notes_delete
+from notes_edit import notes_edit
+from notes_list import notes_list
+
+print("Добрый день, введите, пожалуйста команду для далььнейшей работы с заметками\n"
+      "list - Вывод всех заметок\n"
+      "add_New - создание нового файла для заметок (ЗАПИСЫВАТЬСЯ ТОЛЬКО ОДН РАЗ, ПРИ ДАЛЬНЕЙШЕМ ИСПОЛЬЗОВАНИИ БУДЕТ ПЕРЕЗАПИСЬ)\n"
+      "add - создание новой заметки, в существующий файл\n"
+      "dateSelect - сортировка по дате\n"
+      "delete - удаление заметки по заголовку\n"
+      "edit - редактирование заметки\n"
+      )
+command = input("Введите пожалуйста команду: ")
+
+match command:
+    case "list":
+        notes_list()
+    case "add_New":
+        notes_add()
+    case "add":
+        notes_addContinue()
+    case "dateSelect":
+        notes_dateSelect()
+    case "delete":
+        notes_delete()
+    case "edit":
+        notes_edit()
+    case _:
+        print("Warring: Неизвестная команда")
---
+++ notes_list.py
@@ -0,0 +1,5 @@
+import csv
+def notes_list():
+    r = csv.reader(open('example.csv'))
+    lines = list(r)
+    print(lines)
